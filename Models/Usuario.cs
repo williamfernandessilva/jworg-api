0b0b4678b3a38625af3d2f996ecf9dc735a55105
williamfernandessilva <willgsvs@hotmail.com>
2026-03-18 02:54:03 -0300

    FIaaXxx

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -2,11 +2,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace JworgApi.Models
 {
-    [Table("usuarios")] // Força o C# a procurar a tabela 'usuarios' em minúsculo
+    [Table("usuarios")] 
     public class Usuario
     {
         [Column("id")]
-public string Id { get; set; }
+        public string Id { get; set; } = string.Empty; // DEIXE APENAS ESTE ID
 
         [Column("nome")]
         public string Nome { get; set; } = string.Empty;
@@ -17,7 +17,7 @@ public string Id { get; set; }
         [Column("senha")]
         public string Senha { get; set; } = string.Empty;
         
-       [Column("id")]
-public string Id { get; set; } = string.Empty;
+        [Column("isAdmin")] 
+        public bool IsAdmin { get; set; }
     }
 }
