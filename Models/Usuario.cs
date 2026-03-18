1c00318aab798f890d47602196f8d562ca10aaa8
williamfernandessilva <willgsvs@hotmail.com>
2026-03-18 01:24:39 -0300

    FIX: Liberando CORS para qualquer origem!

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -1,12 +1,23 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace JworgApi.Models
 {
+    [Table("usuarios")] // Força o C# a procurar a tabela 'usuarios' em minúsculo
     public class Usuario
     {
+        [Column("id")]
         public int Id { get; set; }
+
+        [Column("nome")]
         public string Nome { get; set; } = string.Empty;
+
+        [Column("email")]
         public string Email { get; set; } = string.Empty;
+
+        [Column("senha")]
         public string Senha { get; set; } = string.Empty;
         
+        [Column("IsAdmin")] // Se no seu Railway estiver 'is_admin', mude aqui para "is_admin"
         public bool IsAdmin { get; set; }
     }
 }
