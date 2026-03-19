0186ae6c03b949728b1602bc7b8dfc2befd6a540
williamfernandessilva <willgsvs@hotmail.com>
2026-03-18 23:45:04 -0300

    achoaa

--- Models/Bairro.cs
+++ Models/Bairro.cs
@@ -2,24 +2,24 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace JworgApi.Models
 {
-    [Table("bairros")] 
-    public class Bairro
-    {
-        [Column("id")]
-        public int Id { get; set; }
+   [Table("bairros")] 
+public class Bairro
+{
+    [Column("id")]
+    public int Id { get; set; }
 
-        [Column("nome")]
-        public string Nome { get; set; } = string.Empty;
+    [Column("nome")]
+    public string Nome { get; set; } = string.Empty;
 
-        // Forçamos o tipo da coluna para decimal(18,10) ou double para não dar erro 500
-        [Column("lat", TypeName = "double")]
-        public double Lat { get; set; }
+    // Mudamos para string para parar de dar o erro de "Cast"
+    [Column("lat")]
+    public string Lat { get; set; } = "0";
 
-        [Column("lng", TypeName = "double")]
-        public double Lng { get; set; }
+    [Column("lng")]
+    public string Lng { get; set; } = "0";
 
-        [Column("status")]
-        public string? Status { get; set; } = "verde";
+    [Column("status")]
+    public string? Status { get; set; }
 
         [Column("trabalhadoPor")]
         public string? TrabalhadoPor { get; set; }
